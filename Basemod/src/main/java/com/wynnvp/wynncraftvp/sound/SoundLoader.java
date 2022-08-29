5c5c41fa9c06504f16cbf4618035c353a71e6ef2
KingTimer12 <queentimer12@gmail.com>
2022-08-29 20:04:13 -0300

    😳 My directory folder has been exposed (removed)

--- Basemod/src/main/java/com/wynnvp/wynncraftvp/sound/SoundLoader.java
+++ Basemod/src/main/java/com/wynnvp/wynncraftvp/sound/SoundLoader.java
@@ -4,20 +4,12 @@ import com.wynnvp.wynncraftvp.ModCore;
 import com.wynnvp.wynncraftvp.utils.Utils;
 
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.Set;
 
 import static com.wynnvp.wynncraftvp.utils.Utils.copy;
 
 public class SoundLoader {
-
-    //kingsrecruit-aledar-1.ogg
 
     public static void loadSounds(Set<String> ids) {
         ids.forEach(id -> {
@@ -26,7 +18,7 @@ public class SoundLoader {
                     File parent = new File(Utils.FILE_ROOT, getQuest(id));
                     if (!parent.exists())
                         parent.mkdir();
-                    //C:\Users\ender\AppData\Roaming\.minecraft\wynnvp
+
                     File destination = new File(parent, id+".ogg");
                     if (!destination.exists()) {
                         copy(inputStream, destination.getPath());
